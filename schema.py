60b0ba5d0ad90eaf154d7f37e6b8c9793b971adf
Marcin Baranek <55330253+MarcinBaranek@users.noreply.github.com>
2022-06-04 13:29:25 +0200

    Update schema.py

--- schema.py
+++ schema.py
@@ -33,13 +33,43 @@ def euler_step(point, time, dt, a_func, b_func, state):
 
 
 @cuda.jit(device=True)
-def euler_path(initial_point, t_0, end_time, n, a_func, b_func, state):
+def euler_path(initial_point, t_0, end_time, n, a_func, b_func, state, result):
+    tmp = cuda.local.array(shape=(D, 1),
+                           dtype=initial_point.dtype)
+    for i in range(tmp.shape[0]):
+        for j in range(tmp.shape[1]):
+            tmp[i, j] = initial_point[i, j]
     dt = (end_time - t_0) / n
     time = t_0
     while time < end_time:
         # override initial_point
-        euler_step(initial_point, time, dt, a_func, b_func, state)
+        euler_step(tmp, time, dt, a_func, b_func, state)
         time += dt
+    for i in range(tmp.shape[0]):
+        result[i, 0] = tmp[i, 0]
+
+
+def get_kernel_euler_path(a_func, b_func):
+    @cuda.jit
+    def kernel_euler_path(initial_point, time, state, result,):
+        tx = cuda.threadIdx.x
+        ty = cuda.blockIdx.x
+        bw = cuda.blockDim.x
+        pos = tx + ty * bw
+        if pos < result.size / initial_point.shape[0] / time.shape[0]:
+            tmp = cuda.local.array(shape=(D, 1),
+                                   dtype=initial_point.dtype)
+            for i in range(tmp.shape[0]):
+                for j in range(tmp.shape[1]):
+                    tmp[i, j] = initial_point[i, j]
+            for i in range(tmp.shape[0]):
+                result[i + initial_point.shape[0] * pos, 0] = tmp[i, 0]
+            for i in range(time.shape[0] - 1):
+                dt = time[i + 1, 0] - time[i, 0]
+                euler_step(tmp, time[i, 0], dt, a_func, b_func, state)
+                for j in range(tmp.shape[0]):
+                    result[j + initial_point.shape[0] * pos, i + 1] = tmp[j, 0]
+    return kernel_euler_path
 
 
 # ===============================
@@ -62,10 +92,8 @@ def euler_step_with_w(point, time, dt, a_func, b_func, wiener, state):
     drift_step(point, time, dt, a_func, temp_drift)
     diffusion_step_with_w(point, time, dt, b_func,
                           temp_diffusion, wiener, state)
-    point[1, 0] = temp_drift[1, 0]
-    point[2, 0] = temp_diffusion[1, 0]
-    # add_inplace(point, temp_drift)
-    # add_inplace(point, temp_diffusion)
+    add_inplace(point, temp_drift)
+    add_inplace(point, temp_diffusion)
 
 
 @cuda.jit(device=True)
@@ -90,21 +118,21 @@ def strong_euler_path(initial_point, t_0, end_time, n, a_func,
         temp[i, 0] = initial_point[i, 0]
     for i in range(M):
         wiener[i, 0] = 0.0
+        dw[i, 0] = 0.0
     dt = (end_time - t_0) / n
     time = t_0 + dt
     total_err = 0
 
     while time < end_time:
-        euler_step_with_w(temp_point, time, dt, a_func, b_func, dw, state)
-        # add_inplace(wiener, dw)
-        # exact(initial_point, time, wiener, temp)
-        # total_err += sse(temp, temp_point)
-        # time += dt
-        initial_point[0, 0] = wiener[0, 0]
-        initial_point[1, 0] = wiener[1, 0]
-        initial_point[2, 0] = dw[2, 0]
-        break
-    # return total_err / n
 
-    return wiener[4, 0]
+        euler_step_with_w(temp_point, time, dt, a_func, b_func, dw, state)
+        # initial_point[1, 0] = temp_point[1, 0]
+
+        add_inplace(wiener, dw)
+        exact(initial_point, time, wiener, temp)
+
+        total_err += sse(temp, temp_point)
+        time += dt
+    return total_err / n
+
 
